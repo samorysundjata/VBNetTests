3d0989e36072c03e94422970a4745a3af9c74737
Samory Sundjata <samory.sundjata@gmail.com>
2025-07-29 17:25:51 -0300

    Entities

---
+++ src/VBNet.Domain/Entities/Produto.vb
@@ -0,0 +1,17 @@
+﻿Public Class Produto
+
+    Public Property Id As Integer
+    Public Property Nome As String
+    Public Property Preco As Decimal
+    Public Property QuantidadeEmEstoque As Integer
+    Public Property Categoria As String
+
+    Public Sub New(id As Integer, nome As String, preco As Decimal, quantidadeEmEstoque As Integer, categoria As String)
+        Me.Id = id
+        Me.Nome = nome
+        Me.Preco = preco
+        Me.QuantidadeEmEstoque = quantidadeEmEstoque
+        Me.Categoria = categoria
+    End Sub
+
+End Class
